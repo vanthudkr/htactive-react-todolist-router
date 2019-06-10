1e4e368d750a7de1381b068fae24fde5d35bcd80
vanthudkr <thu.ho@student.passerellesnumeriques.org>
2019-06-10 10:49:38 +0700

    Updates

---
+++ precache-manifest.e8157ae0cefdd6a92d7067f0b29ad0ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "001025fef75868ab59f6322dd664e84d",
+    "url": "/htactive-react-todolist-router/index.html"
+  },
+  {
+    "revision": "cfcbe50b3fd9bf1f6fa9",
+    "url": "/htactive-react-todolist-router/static/css/main.3aeede75.chunk.css"
+  },
+  {
+    "revision": "8c2b47b8e0d50bbaa2f2",
+    "url": "/htactive-react-todolist-router/static/js/2.478f2e62.chunk.js"
+  },
+  {
+    "revision": "cfcbe50b3fd9bf1f6fa9",
+    "url": "/htactive-react-todolist-router/static/js/main.f550d50d.chunk.js"
+  },
+  {
+    "revision": "bca0247b834b47858b0f",
+    "url": "/htactive-react-todolist-router/static/js/runtime~main.9ece2110.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/htactive-react-todolist-router/precache-manifest.e8157ae0cefdd6a92d7067f0b29ad0ae.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/htactive-react-todolist-router/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
